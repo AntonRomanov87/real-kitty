1c612ef5654783354a72bac5d078b7955de94ee3
Anton Romanov <anton.romanov.87@gmail.com>
2022-05-21 00:06:02 +0300

    .env

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+  const RealKittyNFTContract = await hre.ethers.getContractFactory("RealKittyNFT");
+  const nftDeployed = await RealKittyNFTContract.deploy("Hello, Hardhat!");
+
+  await nftDeployed.deployed();
+
+  console.log("RealKittyNFT deployed to:", nftDeployed.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
