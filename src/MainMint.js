6ffe3617be0d60aea32246f34c3013e6720d2a3c
Anton Romanov <anton.romanov.87@gmail.com>
2022-05-21 01:48:32 +0300

    Стилизация

--- src/MainMint.js
+++ src/MainMint.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { ethers, BigNumber } from 'ethers';
+import { Box, Button, Flex, Input, Text } from '@chakra-ui/react';
 import realKittyNFT from './RealKittyNFT.json';
 
 const realKittyNFTAddress = "0x?????????????????????????????????????????????????";
@@ -42,22 +43,79 @@ const MainMint = ({ accounts, setAccounts }) => {
     }
 
     return (
-        <div>
-            <h1>Real Kitty NFT</h1>
-            <p>Real kitty wants to be famous. Mint it!</p>
-            {isConnected ? (
+        <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
+            <Box width="520px">
                 <div>
-                    <div>
-                        <button onClick={handeDecrement}>-</button>
-                        <input type="number" value={mintAmount} readOnly />
-                        <button onClick={handeIncrement}>+</button>
-                    </div>
-                    <button onClick={handleMint}> Mint now </button>
+                    <Text fontSize="48px" textShadow="0 5px #000000">RealKittyNFT</Text>
+                    <Text
+                        fontSize="30px"
+                        letterSpacing="-5.5%"
+                        fontFamily="VT323"
+                        textShadow="0 2px 2px #000000"
+                        >
+                        Real kitty wants to be famous. Mint it!
+                    </Text>
                 </div>
-            ) : (
-                <p>You have to connect metamask wallet to mint.</p>
-            )}
-        </div>
+                
+                {isConnected ? (
+                    <div>
+                        <Flex align="center" justify="center">
+                            <Button 
+                                backgroundColor="#D6517D"
+                                borderRadius="Spx"
+                                boxShadow="0px 2px 2px 1px #0F0F0F"
+                                color="white"
+                                cursor="pointer"
+                                fontFamily="inherit"
+                                padding="15px"
+                                marginTop="15px"
+                                onClick={handeDecrement}>-</Button>
+                            <Input 
+                                readOnly
+                                fontFamily="inherit"
+                                width="100px"
+                                height="40px"
+                                textAlign="center"
+                                paddingLeft="19px"
+                                marginTop="10px"
+                                type="number" 
+                                value={mintAmount}  />
+                            <Button
+                                 backgroundColor="#D6517D"
+                                 borderRadius="Spx"
+                                 boxShadow="0px 2px 2px 1px #0F0F0F"
+                                 color="white"
+                                 cursor="pointer"
+                                 fontFamily="inherit"
+                                 padding="15px"
+                                 marginTop="15px" 
+                                onClick={handeIncrement}>+</Button>
+                        </Flex>
+                        <Button 
+                            backgroundColor="#D6517D"
+                            borderRadius="Spx"
+                            boxShadow="0px 2px 2px 1px #0F0F0F"
+                            color="white"
+                            cursor="pointer"
+                            fontFamily="inherit"
+                            padding="15px"
+                            marginTop="15px" 
+                            onClick={handleMint}> Mint now </Button>
+                    </div>
+                ) : (
+                    <Text
+                        marginTop="70px"
+                        fontSize="30px"
+                        letterSpacing="-5.5%"
+                        fontFamily="VT323"
+                        textShadow="0 3px #000000"
+                        color="#D6517D"
+                        >
+                        You have to connect metamask wallet to mint.
+                    </Text>
+                )}
+            </Box>
+        </Flex>
     );
 };
 
